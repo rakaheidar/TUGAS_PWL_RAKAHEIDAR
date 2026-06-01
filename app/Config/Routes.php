3a02220458adf6b7bd3ec6384c0215f2e1460f60
rakaheidar <111202314967@mhs.dinus.ac.id>
2026-06-02 03:09:37 +0700

    Mengamankan konfigurasi routes dan produk controller

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -12,7 +12,13 @@ use CodeIgniter\Router\RouteCollection;
  $routes->post('login', 'AuthController::login');
  $routes->get('logout', 'AuthController::logout');
  
- $routes->get('produk', 'ProdukController::index', ['filter' => 'auth']);
+ $routes->group('produk', ['filter' => 'auth'], function ($routes) { 
+    $routes->get('', 'ProdukController::index');
+    $routes->post('', 'ProdukController::create');
+    $routes->post('edit/(:any)', 'ProdukController::edit/$1');
+    $routes->get('delete/(:any)', 'ProdukController::delete/$1');
+});
+ 
  $routes->get('keranjang', 'TransaksiController::index', ['filter' => 'auth']);
  $routes->get('profil','Profil::index', ['filter' => 'auth']);
 
